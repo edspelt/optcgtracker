41b63e81175bbc0d838f3588d6eb570200092ca5
Spelito <spelito@pecesito1.local>
2025-02-10 14:08:29 -0300

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['your-domain.com'],
+    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
     unoptimized: process.env.NODE_ENV !== 'production'
   },
   experimental: {
